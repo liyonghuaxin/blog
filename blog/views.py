ae2b7b72fa1220588d39094fe9d0d031cf75f988
liyonghuaxin <1217468488@qq.com>
2018-04-22 12:18:49 +0800

    'markdown'

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Blog
+import markdown
 
 # Create your views here.
 def index(request):
     # blog = Blog.objects.all()
     blog = Blog.objects.all().first()
-    return render(request, 'blog.html', {'blog': blog})
+    blog_content = markdown.markdown(blog.content)
+    return render(request, 'blog.html', {'blog' : blog, 'blog_content': blog_content})
     # return render(request, 'blog.html')
     # return HttpResponse("Blog's blog")
