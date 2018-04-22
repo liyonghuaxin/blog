acca6f475635be15baff9dddc90eeea8738f58d3
liyonghuaxin <1217468488@qq.com>
2018-04-22 12:53:30 +0800

    'blog'

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length = 64)
     content = models.TextField()
+
+    def __str__(self):
+        return "标题：{}, 字数：{}字, 概要：{}......".format(self.title, len(self.content), self.content[:10])
